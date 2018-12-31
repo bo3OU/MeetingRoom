4f08e2b69f8ab45cfd921ee3b77c2792c3a031b8
ali bagho <ali.bagho@gmail.com>
2018-12-31 15:43:09 +0100

    localhost to meetingrooms.tk

--- src/Login.js
+++ src/Login.js
@@ -20,7 +20,7 @@ export default class  Login extends Component {
                 'top': '40%'
             }}>
 
-            <a href="https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=113603925707-l0gumcbmfrisq3vtkgi83g5oc1eiiiqv.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Froomlist&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar.events+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar.events.readonly+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar.readonly+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar.settings.readonly&access_type=offline&approvalPrompt=force" 
+            <a href="https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=113603925707-l0gumcbmfrisq3vtkgi83g5oc1eiiiqv.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Fmeetingrooms.tk%3A3000%2Froomlist&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar.events+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar.events.readonly+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar.readonly+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar.settings.readonly&access_type=offline&approvalPrompt=force" 
             title="Google+" 
             class="btn btn-googleplus btn-lg">
                 <i class="fa fa-google-plus fa-fw"></i> 
